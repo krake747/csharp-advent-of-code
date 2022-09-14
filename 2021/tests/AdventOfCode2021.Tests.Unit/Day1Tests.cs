072a4d79a23ee447458053fc0c8ccf5ede63c505
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-09-14 13:45:16 +0200

    ♻️ (refactor(tests): Split up main file.

---
+++ 2021/tests/AdventOfCode2021.Tests.Unit/Day1Tests.cs
@@ -0,0 +1,33 @@
+using Xunit;
+using FluentAssertions;
+
+namespace AdventOfCode2021.Tests.Unit;
+
+public class Day1Tests
+{
+    [Theory]
+    [InlineData(new int[] { }, 0)]
+    [InlineData(new int[] { 199 }, 0)]
+    [InlineData(new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 }, 7)]
+    public void Part1_ShouldReturnInteger_WhenEnumberableIsInteger(int[] values, int expected)
+    {
+        // Act
+        var result = Day1.Part1(values);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [InlineData(new int[] { }, 0)]
+    [InlineData(new int[] { 199 }, 0)]
+    [InlineData(new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 }, 5)]
+    public void Part2__ShouldReturnInteger_WhenEnumberableIsInteger(int[] values, int expected)
+    {
+        // Act
+        var result = Day1.Part2(values);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
--- 2021/tests/AdventOfCode2021.Tests.Unit/Day2Tests.cs
+++ 2021/tests/AdventOfCode2021.Tests.Unit/Day2Tests.cs
@@ -3,34 +3,8 @@ using FluentAssertions;
 
 namespace AdventOfCode2021.Tests.Unit;
 
-public class AdventOfCode2021Tests
+public class Day2Tests
 {
-    [Theory]
-    [InlineData(new int[] { }, 0)]
-    [InlineData(new int[] { 199 }, 0)]
-    [InlineData(new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 }, 7)]
-    public void Day1_Part1_ShouldReturnInteger_WhenEnumberableIsInteger(int[] values, int expected)
-    {
-        // Act
-        var result = Day1.Part1(values);
-
-        // Assert
-        result.Should().Be(expected);
-    }
-
-    [Theory]
-    [InlineData(new int[] { }, 0)]
-    [InlineData(new int[] { 199 }, 0)]
-    [InlineData(new int[] { 199, 200, 208, 210, 200, 207, 240, 269, 260, 263 }, 5)]
-    public void Day1_Part2__ShouldReturnInteger_WhenEnumberableIsInteger(int[] values, int expected)
-    {
-        // Act
-        var result = Day1.Part2(values);
-
-        // Assert
-        result.Should().Be(expected);
-    }
-
     [Theory]
     [InlineData(new string[] { }, 0)]
     [InlineData(new string[] { "forward 5" }, 0)]
