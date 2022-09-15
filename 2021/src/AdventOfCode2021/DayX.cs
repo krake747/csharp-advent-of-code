62a3b73aa915e407bc2590943b957bbf6aab6371
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-09-15 21:17:00 +0200

    🌱 files: Add template classes.

---
+++ 2021/src/AdventOfCode2021/DayX.cs
@@ -0,0 +1,14 @@
+﻿namespace AdventOfCode2021;
+
+public static class DayX
+{
+    public static int Part1(IEnumerable<string> input)
+    {
+        return 1;
+    }
+
+    public static int Part2(IEnumerable<string> input)
+    {
+        return 1;
+    }
+}
---
+++ 2021/tests/AdventOfCode2021.Tests.Unit/DayXTests.cs
@@ -0,0 +1,37 @@
+﻿//using System.Collections;
+//using FluentAssertions;
+//using Xunit;
+
+//namespace AdventOfCode2021.Tests.Unit;
+
+//public class DayXTests
+//{
+//    [Theory]
+//    [ClassData(typeof(DayXPart1TestData))]
+//    public void Part1_ShouldReturnInteger_WhenEnumberableAreStrings(string[] values, int expected)
+//    {
+//        // Act
+//        var result = DayX.Part1(values);
+
+//        // Assert 
+//        result.Should().Be(expected);
+//    }
+//}
+
+
+//public class DayXPart1TestData : IEnumerable<object[]>
+//{
+//    public IEnumerator<object[]> GetEnumerator()
+//    {
+//        yield return new object[]
+//        {
+//            new string[]
+//            {
+//                "0,9 -> 5,9",
+//            },
+//            5
+//        };
+//    }
+
+//    IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+//}
