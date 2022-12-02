9b1dc9a6f664570c742699c9f916d812ef07cf6e
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-12-02 18:34:05 +0100

    Final Test Github Actions

--- 2022/src/AdventOfCode2022/Day01.cs
+++ 2022/src/AdventOfCode2022/Day01.cs
@@ -3,21 +3,15 @@ using AdventOfCodeLib;
 
 namespace AdventOfCode2022;
 
-public class Day01 : IDay<IEnumerable<string>>
+public class Day01 : IDay<string>
 {
-    public int Part1(IEnumerable<string> input)
-    {
-        return TotalCaloriesPerElf(input)
-            .Max();
-    }
-    
     public int Part1(string input)
     {
         return TotalCaloriesPerElf(input)
             .Max();
     }
 
-    public int Part2(IEnumerable<string> input)
+    public int Part2(string input)
     {
         return TotalCaloriesPerElf(input)
             .OrderDescending()
@@ -25,19 +19,9 @@ public class Day01 : IDay<IEnumerable<string>>
             .Sum();
     }
 
-    private static IEnumerable<int> TotalCaloriesPerElf(IEnumerable<string> calories)
-    {
-        return string.Join("|", calories)
-            .Split("||")
-            .Select(inventory => inventory.Split("|")
-                .Select(int.Parse)
-                .Sum());
-    }
-    
     private static IEnumerable<int> TotalCaloriesPerElf(string calories)
     {
-        return calories
-            .Split("\n\n")
+        return calories.Split("\n\n")
             .Select(inventory => inventory.Split("\n")
                 .Select(int.Parse)
                 .Sum());
--- 2022/tests/AdventOfCode2022.Tests.Unit/Day01Tests.cs
+++ 2022/tests/AdventOfCode2022.Tests.Unit/Day01Tests.cs
@@ -14,31 +14,26 @@ public class Day01Tests
         _sut = new Day01();
     }
 
-    private static IEnumerable<string> TestData => FetchFile(@"..\..\..\Data\Day01_Test.txt", ReadAsEnumerable);
-    private static IEnumerable<string> RealData => FetchFile(@"..\..\..\Data\Day01.txt", ReadAsEnumerable);
-    private static string TestData1 => FetchFile(@"..\..\..\Data\Day01_Test.txt", ReadAsString);
+    private static string TestData => FetchFile(@"..\..\..\Data\Day01_Test.txt", ReadAsString);
+    private static string RealData => FetchFile(@"..\..\..\Data\Day01.txt", ReadAsString);
 
-    public static TheoryData<IEnumerable<string>, int> Part1Data => new()
+    public static TheoryData<string, int> Part1Data => new()
     {
         { TestData, 24000 },
         { RealData, 69795 }
     };
 
-    public static TheoryData<IEnumerable<string>, int> Part2Data => new()
+    public static TheoryData<string, int> Part2Data => new()
     {
         { TestData, 45000 },
         { RealData, 208437 }
     };
-    
-    public static TheoryData<string, int> Part1Test => new()
-    {
-        { TestData1, 24000 },
-    };
+
 
     [Theory]
     [MemberData(nameof(Part1Data))]
     [Description("How many total Calories is that Elf carrying?")]
-    public void Part1_ShouldReturnInteger_WhenSearchingTheElfCarryingTheMostCalories(IEnumerable<string> values, int expected)
+    public void Part1_ShouldReturnInteger_WhenSearchingTheElfCarryingTheMostCalories(string values, int expected)
     {
         // Act
         var result = _sut.Part1(values);
@@ -50,23 +45,11 @@ public class Day01Tests
     [Theory]
     [MemberData(nameof(Part2Data))]
     [Description("How many Calories are those Elves carrying in total?")]
-    public void Part2_ShouldReturnInteger_WhenSearchingTheTopThreeElvesCarryingTheMostCalories(IEnumerable<string> values,
+    public void Part2_ShouldReturnInteger_WhenSearchingTheTopThreeElvesCarryingTheMostCalories(string values,
         int expected)
     {
         // Act
         var result = _sut.Part2(values);
-
-        // Assert
-        result.Should().Be(expected);
-    }
-    
-    [Theory]
-    [MemberData(nameof(Part1Test))]
-    [Description("How many total Calories is that Elf carrying?")]
-    public void Part1Test_ShouldReturnInteger_WhenSearchingTheElfCarryingTheMostCalories(string values, int expected)
-    {
-        // Act
-        var result = _sut.Part1(values);
 
         // Assert
         result.Should().Be(expected);
