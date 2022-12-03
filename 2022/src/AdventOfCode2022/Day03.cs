24d5326eedd315688f348f71ac97a6798bad95b0
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-12-03 14:00:59 +0100

    ♻ refactor(2022): Day 3. Parse item priority value

--- 2022/src/AdventOfCode2022/Day03.cs
+++ 2022/src/AdventOfCode2022/Day03.cs
@@ -5,22 +5,18 @@ namespace AdventOfCode2022;
 
 public class Day03 : IDay<IEnumerable<string>>
 {
-    private const string Alphabet = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
-    
     public int Part1(IEnumerable<string> input)
     {
-        var priorities = CreatePriorities(Alphabet);
         return input.Select(CreateTwoCompartments)
             .Select(DistinctItemFromBothCompartments)
-            .Sum(item => priorities[item]);
+            .Sum(ParseItemPriorityValue);
     }
-    
+
     public int Part2(IEnumerable<string> input)
     {
-        var priorities = CreatePriorities(Alphabet);
         return input.GroupBackpacksBy(3)
             .Select(DistinctItemFromGroupOfBackpacks)
-            .Sum(item => priorities[item]);
+            .Sum(ParseItemPriorityValue);
     }
 
     private static Dictionary<char, int> CreatePriorities(string alphabet)
@@ -34,7 +30,7 @@ public class Day03 : IDay<IEnumerable<string>>
     {
         return new Compartment(backpack[..(backpack.Length / 2)], backpack[(backpack.Length / 2)..]);
     }
-    
+
     private static char DistinctItemFromBothCompartments(Compartment compartment)
     {
         return compartment.First.Intersect(compartment.Second).First();
@@ -44,7 +40,12 @@ public class Day03 : IDay<IEnumerable<string>>
     {
         var input = backpacks.ToArray();
         return input.Skip(1).Aggregate(ImmutableHashSet.Create(input.First().ToCharArray()), (h, e) => h.Intersect(e))
-                .First();
+            .First();
+    }
+
+    private static int ParseItemPriorityValue(char c)
+    {
+        return c < 'a' ? c - 'A' + 27 : c - 'a' + 1;
     }
 
     private record Compartment(string First, string Second);
@@ -52,13 +53,11 @@ public class Day03 : IDay<IEnumerable<string>>
 
 internal static class Day03Extensions
 {
-    internal static IEnumerable<IEnumerable<string>> GroupBackpacksBy(this IEnumerable<string> backpacks, 
+    internal static IEnumerable<IEnumerable<string>> GroupBackpacksBy(this IEnumerable<string> backpacks,
         int count)
     {
         var input = backpacks.ToArray();
         for (var backpack = 0; backpack < input.Length; backpack += count)
-        {
             yield return input.Skip(backpack).Take(count);
-        }
     }
 }
