cefe841d93b333237cd3d5c26e59934d63bfafab
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-12-06 10:47:48 +0100

    ✨ feat(2022): Solve Day 6 - Part 1

---
+++ src/2022/AdventOfCode2022/Day06.cs
@@ -0,0 +1,27 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCodeLib;
+
+namespace AdventOfCode2022;
+
+public class Day06 : IDay<string, int>
+{
+    public int Part1(string input)
+    {
+        const int window = 4;
+        return Enumerable.Range(0, input.Length - window + 1)
+            .First(n => input.IsStartOfMarker(n, window)) + window;
+    }
+
+    public int Part2(string input)
+    {
+        return 1;
+    }
+}
+
+internal static class Day06Extensions
+{
+    internal static bool IsStartOfMarker(this string source, int index ,int window)
+    {
+        return source.Skip(index).Take(window).Distinct().ToArray().Length == window;
+    }
+}
---
+++ tests/2022/AdventOfCode2022.Tests.Unit/Day06Tests.cs
@@ -0,0 +1,57 @@
+﻿using System.ComponentModel;
+using static AdventOfCodeLib.TextFileReaderService;
+
+// ReSharper disable StringLiteralTypo
+
+namespace AdventOfCode2022.Tests.Unit;
+
+[Description("Day 06 - Tuning Trouble")]
+public class Day06Tests
+{
+    private readonly Day06 _sut;
+
+    public Day06Tests()
+    {
+        // Arrange
+        _sut = new Day06();
+    }
+
+    private static string TestData => FetchFile(@"..\..\..\Data\Day06_Test.txt", ReadAsString);
+    private static string RealData => FetchFile(@"..\..\..\Data\Day06.txt", ReadAsString);
+
+    public static TheoryData<string, int> Part1Data => new()
+    {
+        { TestData, 7 },
+        { RealData, 1766 }
+    };
+
+    public static TheoryData<string, int> Part2Data => new()
+    {
+        { TestData, 1 },
+        { RealData, 1 }
+    };
+
+    [Theory]
+    [MemberData(nameof(Part1Data))]
+    [Description("How many characters need to be processed before the first start-of-packet marker is detected?")]
+    public void Part1_ShouldReturnInteger(string values, int expected)
+    {
+        // Act
+        var result = _sut.Part1(values);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(Part2Data))]
+    [Description("How many characters need to be processed before the first start-of-packet marker is detected?")]
+    public void Part2_ShouldReturnInteger(string values, int expected)
+    {
+        // Act
+        var result = _sut.Part2(values);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
