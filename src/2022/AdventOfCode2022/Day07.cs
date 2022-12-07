96ca205f9ef02e0088540bdf5fa23aaa512282d1
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-12-08 00:17:03 +0100

    ✨ feat(2022): solve day 7 part 1

---
+++ src/2022/AdventOfCode2022/Day07.cs
@@ -0,0 +1,44 @@
+﻿using System.Text.RegularExpressions;
+using AdventOfCodeLib;
+using AdventOfCodeLib.Interfaces;
+
+namespace AdventOfCode2022;
+
+[AocPuzzle(2022, 7, "No Space Left On Device")]
+public class Day07 : IDay<IEnumerable<string>, int>
+{
+    public int Part1(IEnumerable<string> input)
+    {
+        var paths = new Stack<string>();
+        var directories = new Dictionary<string, int>{ {"/", 0} };
+
+        foreach (var line in input)
+        {
+            var parameters = line.Split(' ');
+            if (Regex.IsMatch(line, @"^\$ cd (\w+|/)"))
+            {
+                paths.Push(string.Concat(paths) + parameters[^1]);
+            }
+            else if (Regex.IsMatch(line, @"^\$ cd (..)"))
+            {
+                paths.Pop();
+            }
+            else if (Regex.IsMatch(line, @"^(\d+)"))
+            {
+                var size = int.Parse(parameters[0]);
+                foreach (var parent in paths)
+                {
+                    directories[parent] = directories.GetValueOrDefault(parent) + size;
+                }
+            }
+        }
+        
+        return directories.Where(kvp => kvp.Value < 100000).Sum(kvp => kvp.Value);
+    }
+
+    public int Part2(IEnumerable<string> input)
+    {
+        return 1;
+    }
+
+}
---
+++ tests/2022/AdventOfCode2022.Tests.Unit/Day07Tests.cs
@@ -0,0 +1,55 @@
+﻿using System.ComponentModel;
+using static AdventOfCodeLib.TextFileReaderService;
+
+namespace AdventOfCode2022.Tests.Unit;
+
+[Description("Day 07 - No Space Left On Device")]
+public class Day07Tests
+{
+    private readonly Day07 _sut;
+
+    public Day07Tests()
+    {
+        // Arrange
+        _sut = new Day07();
+    }
+
+    private static IEnumerable<string> TestData => FetchFile(@"..\..\..\Data\Day07_Test.txt", ReadAsEnumerable);
+    private static IEnumerable<string> RealData => FetchFile(@"..\..\..\Data\Day07.txt", ReadAsEnumerable);
+
+    public static TheoryData<IEnumerable<string>, int> Part1Data => new()
+    {
+        { TestData, 95437 },
+        { RealData, 1543140 }
+    };
+
+    public static TheoryData<IEnumerable<string>, int> Part2Data => new()
+    {
+        { TestData, 4 },
+        { RealData, 914 }
+    };
+
+    [Theory]
+    [MemberData(nameof(Part1Data))]
+    [Description("Total size of at most 100000. What is the sum of the total sizes of those directories?")]
+    public void Part1_ShouldReturnInteger(IEnumerable<string> values, int expected)
+    {
+        // Act
+        var result = _sut.Part1(values);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(Part2Data))]
+    [Description("Total size of at most 100000. What is the sum of the total sizes of those directories?")]
+    public void Part2_ShouldReturnInteger(IEnumerable<string> values, int expected)
+    {
+        // Act
+        var result = _sut.Part2(values);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
