520e511616a3823714ecbb20831a310781940739
Kevin Kraemer <kraemer.kevin747@gmail.com>
2022-12-18 13:28:30 +0100

    ✨ feat(2022): solve day 18 part 1

---
+++ src/2022/AdventOfCode2022/Day18.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using AdventOfCodeLib;
+using AdventOfCodeLib.Interfaces;
+
+namespace AdventOfCode2022;
+
+[AocPuzzle(2022, 18, "Boiling Boulders")]
+public sealed class Day18 : IDay<IEnumerable<string>, int>
+{
+    public int Part1(IEnumerable<string> input)
+    {
+        var lavaLocations = LavaLocations(input).ToHashSet();
+        return lavaLocations.SelectMany(Neighbors)
+            .Count(lavaLocation => !lavaLocations.Contains(lavaLocation));
+    }
+
+    public int Part2(IEnumerable<string> input)
+    {
+        return 1;
+    }
+
+    private static IEnumerable<Point> LavaLocations(IEnumerable<string> input)
+    {
+        return input.Select(line => line.Split(',').Select(int.Parse).ToArray())
+            .Select(line =>new Point(line[0], line[1], line[2]));
+    }
+
+    private static IEnumerable<Point> Neighbors(Point lavaLocation)
+    {
+        return new[]
+        {
+            lavaLocation with { X = lavaLocation.X - 1 },
+            lavaLocation with { X = lavaLocation.X + 1 },
+            lavaLocation with { Y = lavaLocation.Y - 1 },
+            lavaLocation with { Y = lavaLocation.Y + 1 },
+            lavaLocation with { Z = lavaLocation.Z - 1 },
+            lavaLocation with { Z = lavaLocation.Z + 1 },
+        };
+    }
+    
+    private readonly record struct Point(int X, int Y, int Z);
+}
---
+++ tests/2022/AdventOfCode2022.Tests.Unit/Day18Tests.cs
@@ -0,0 +1,56 @@
+﻿using System.ComponentModel;
+using AdventOfCodeLib;
+using static AdventOfCodeLib.TextFileReaderService;
+
+namespace AdventOfCode2022.Tests.Unit;
+
+[AocPuzzle(2022, 18, "Boiling Boulders")]
+public class Day18Tests
+{
+    private readonly Day18 _sut;
+
+    public Day18Tests()
+    {
+        // Arrange
+        _sut = new Day18();
+    }
+
+    private static IEnumerable<string> TestData => FetchFile(@"..\..\..\Data\Day18_Test.txt", ReadAsEnumerable);
+    private static IEnumerable<string> RealData => FetchFile(@"..\..\..\Data\Day18.txt", ReadAsEnumerable);
+
+    public static TheoryData<IEnumerable<string>, int> Part1Data => new()
+    {
+        { TestData, 64 },
+        { RealData, 3564 }
+    };
+
+    public static TheoryData<IEnumerable<string>, int> Part2Data => new()
+    {
+        { TestData, 64 },
+        { RealData, 64 }
+    };
+
+    [Theory]
+    [MemberData(nameof(Part1Data))]
+    [Description("What is the surface area of your scanned lava droplet?")]
+    public void Part1_ShouldReturnInteger(IEnumerable<string> values, int expected)
+    {
+        // Act
+        var result = _sut.Part1(values);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+
+    [Theory]
+    [MemberData(nameof(Part2Data))]
+    [Description("What is the surface area of your scanned lava droplet?")]
+    public void Part2_ShouldReturnInteger(IEnumerable<string> values, int expected)
+    {
+        // Act
+        var result = _sut.Part2(values);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+}
