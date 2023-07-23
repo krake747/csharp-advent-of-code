55edaa5971d5d7cdcfb3571629e51f424bf28cd2
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-07-23 23:55:21 +0200

    feat(2015): Add Day 03 [skip ci]

---
+++ src/2015/AdventOfCode2015/Day03.cs
@@ -0,0 +1,11 @@
+﻿using AdventOfCodeLib;
+
+namespace AdventOfCode2015;
+
+[AocPuzzle(2015, 3, "Perfectly Spherical Houses in a Vacuum")]
+public sealed class Day03 : IAocDay<int>
+{
+    public static int Part1(AocInput input) => 1;
+
+    public static int Part2(AocInput input) => 1;
+}
