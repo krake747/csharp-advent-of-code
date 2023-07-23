4fabf3fa811d9424bfaaf033dbb9fa98b51fd55c
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-07-23 23:32:35 +0200

    refactor(2015): Day 2

--- src/2015/AdventOfCode2015/Day02.cs
+++ src/2015/AdventOfCode2015/Day02.cs
@@ -5,14 +5,16 @@ namespace AdventOfCode2015;
 [AocPuzzle(2015, 2, "I Was Told There Would Be No Math")]
 public sealed class Day02 : IAocDay<int>
 {
-    public static int Part1(AocInput input) => input.Lines
-        .Select(x => x.Split('x').Select(int.Parse).ToArray())
-        .Sum(x => OrderWrappingPaper(x[0], x[1], x[2]));
+    public static int Part1(AocInput input) => SizeUpPresents(input.Lines)
+        .Sum(x => OrderWrappingPaper(x.Length, x.Width, x.Height));
 
-    public static int Part2(AocInput input) => input.Lines
-        .Select(x => x.Split('x').Select(int.Parse).ToArray())
-        .Sum(x => OrderRibbon(x[0], x[1], x[2]));
+    public static int Part2(AocInput input) => SizeUpPresents(input.Lines)
+        .Sum(x => OrderRibbon(x.Length, x.Width, x.Height));
 
+    private static IEnumerable<Present> SizeUpPresents(IEnumerable<string> input) =>
+        input.Select(x => x.Split('x').Select(int.Parse).ToArray())
+            .Select(x => new Present(x[0], x[1], x[2]));
+    
     private static int OrderWrappingPaper(int length, int width, int height) =>
         CalculateSurface(length, width, height) + CalculateAreaOfSmallestSide(length, width, height);
 
@@ -30,4 +32,6 @@ public sealed class Day02 : IAocDay<int>
 
     private static int CalculatePerimeterOfSmallestSide(int length, int width, int height) =>
         new[] { 2 * length + 2 * width, 2 * width + 2 * height, 2 * height + 2 * length }.Min();
+
+    private readonly record struct Present(int Length, int Width, int Height);
 }
