08e729186e49780519aa2fc5dde45475d0940101
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-07-24 21:01:24 +0200

    refactor(2015): day 3

--- src/2015/AdventOfCode2015/Day03.cs
+++ src/2015/AdventOfCode2015/Day03.cs
@@ -9,40 +9,36 @@ public sealed class Day03 : IAocDay<int>
         .Aggregate(new List<Points> { new(0, 0) }, (visited, x) =>
         {
             var currentHouse = visited[^1];
-            var nextHouse = x switch
-            {
-                '^' => currentHouse with { Y = currentHouse.Y + 1 },
-                '>' => currentHouse with { X = currentHouse.X + 1 },
-                'v' => currentHouse with { Y = currentHouse.Y - 1 },
-                '<' => currentHouse with { X = currentHouse.X - 1 },
-                _ => currentHouse
-            };
+            var nextHouse = Points.MoveToNextHouse(x, currentHouse);
             visited.Add(nextHouse);
             return visited;
         })
         .ToHashSet()
         .Count;
-
+    
     public static int Part2(AocInput input) => input.Text
         .Select((x, i) => (Direction: x, Index: i))
         .Aggregate(new List<List<Points>> { new() { new Points(0, 0) }, new() { new Points(0, 0) } }, (visited, x) =>
         {
-            var santa = x.Index % 2 == 0 ? 0 : 1;
-            var currentHouse = visited[santa][^1];
-            var nextHouse = x.Direction switch
-            {
-                '^' => currentHouse with { Y = currentHouse.Y + 1 },
-                '>' => currentHouse with { X = currentHouse.X + 1 },
-                'v' => currentHouse with { Y = currentHouse.Y - 1 },
-                '<' => currentHouse with { X = currentHouse.X - 1 },
-                _ => currentHouse
-            };
-            visited[santa].Add(nextHouse);
+            var isSanta = x.Index % 2 == 0 ? 0 : 1;
+            var currentHouse = visited[isSanta][^1];
+            var nextHouse = Points.MoveToNextHouse(x.Direction, currentHouse);
+            visited[isSanta].Add(nextHouse);
             return visited;
         })
         .SelectMany(x => x)
         .ToHashSet()
         .Count;
 
-    private readonly record struct Points(int X, int Y);
+    private readonly record struct Points(int X, int Y)
+    {
+        internal static Points MoveToNextHouse(char x, Points current) => x switch
+        {
+            '^' => current with { Y = current.Y + 1 },
+            '>' => current with { X = current.X + 1 },
+            'v' => current with { Y = current.Y - 1 },
+            '<' => current with { X = current.X - 1 },
+            _ => current
+        };
+    };
 }
