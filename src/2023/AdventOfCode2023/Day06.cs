f2296113b131e99071c25262c114864b4e934e9f
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-12-06 20:41:00 +0100

    feat(2023): solve day 6 part 2

--- src/2023/AdventOfCode2023/Day06.cs
+++ src/2023/AdventOfCode2023/Day06.cs
@@ -4,34 +4,54 @@ using AdventOfCodeLib;
 namespace AdventOfCode2023;
 
 [AocPuzzle(2023, 6, "Wait For It")]
-public sealed partial class Day06 : IAocDay<int>
+public sealed partial class Day06 : IAocDay<long>
 {
-    public static int Part1(AocInput input) =>
-        ParseRaces(input.Lines)
-            .Select(BeatingTheRecord)
-            .Aggregate(1, (wins, ways) => wins * ways);
+    public static long Part1(AocInput input) =>
+        ParseRaceDocument(input.Lines, ParseBadWriting)
+            .Select(BreakRecord)
+            .Aggregate(1L, (wins, ways) => wins * ways);
 
-    private static int BeatingTheRecord(Race race) =>
-        Enumerable.Range(0, race.Time + 1)
-            .Select(hold => (BoatSpeed: hold, TravelTime: race.Time - hold))
-            .Select(x => x.BoatSpeed * x.TravelTime)
-            .Select(travel => travel > race.RecordDistance)
-            .Count(x => x);
-
-    private static IEnumerable<Race> ParseRaces(IEnumerable<string> lines) => lines
-        .Select(line => NumbersRegex().Matches(line).Select(m => int.Parse(m.Value)))
-        .ToArray()
-        .Pipe(document =>
-        {
-            var times = document[0];
-            var distances = document[^1];
-            return times.Zip(distances, (t, d) => new Race(t, d));
-        });
-
-    public static int Part2(AocInput input) => 0;
+    public static long Part2(AocInput input) =>
+        ParseRaceDocument(input.Lines, ParseGoodWriting)
+            .Select(BreakRecord)
+            .Aggregate(1L, (wins, ways) => wins * ways);
     
+    
+    private static long BreakRecord(Race race)
+    {
+        var count = 0L;
+        for (var charge = 0; charge < race.Time + 1L; charge++)
+        {
+            count += charge * (race.Time - charge) > race.RecordDistance ? 1 : 0;
+        }
+        
+        return count;
+    }
+
+
+    private static IEnumerable<Race> ParseRaceDocument(IEnumerable<string> lines, 
+        Func<IEnumerable<string>[], IEnumerable<Race>> parser) => 
+        lines
+            .Select(line => NumbersRegex().Matches(line).Select(m => m.Value))
+            .ToArray()
+            .Pipe(parser);
+
+    private static IEnumerable<Race> ParseBadWriting(IEnumerable<string>[] document)
+    {
+        var times = document[0].Select(long.Parse);
+        var distances = document[^1].Select(long.Parse);
+        return times.Zip(distances, (t, d) => new Race(t, d));
+    }
+    
+    private static IEnumerable<Race> ParseGoodWriting(IEnumerable<string>[] document)
+    {
+        var time = string.Join("", document[0]).Pipe(long.Parse);
+        var distance = string.Join("", document[^1]).Pipe(long.Parse);
+        return [new Race(time, distance)];
+    }
+
     [GeneratedRegex(@"\d+")]
     private static partial Regex NumbersRegex();
 
-    private readonly record struct Race(int Time, int RecordDistance);
+    private readonly record struct Race(long Time, long RecordDistance);
 }
--- tests/2023/AdventOfCode2023.Tests.Unit/Day06Tests.cs
+++ tests/2023/AdventOfCode2023.Tests.Unit/Day06Tests.cs
@@ -6,29 +6,29 @@ using static AdventOfCodeLib.AocFileReaderService;
 namespace AdventOfCode2023.Tests.Unit;
 
 [AocPuzzle(2023, 6, "Wait For It")]
-public sealed class Day06Tests
+public sealed class Day06Tests : IAocTestDay<long>
 {
     private const string Day = nameof(Day06);
     private const string TestData = @$"..\..\..\Data\{Day}_Test.txt";
     private const string RealData = @$"..\..\..\Data\{Day}.txt";
     private readonly Day06 _sut = new();
 
-    public static TheoryData<AocInput, int> Part1Data => new()
+    public static TheoryData<AocInput, long> Part1Data => new()
     {
         { ReadInput(TestData), 288 },
         { ReadInput(RealData), 281600 }
     };
 
-    public static TheoryData<AocInput, int> Part2Data => new()
+    public static TheoryData<AocInput, long> Part2Data => new()
     {
-        { ReadInput(TestData), 0 },
-        { ReadInput(RealData), 0 }
+        { ReadInput(TestData), 71503 },
+        { ReadInput(RealData), 33875953 }
     };
 
     [Theory]
     [MemberData(nameof(Part1Data))]
     [Description("What do you get if you multiply these numbers together?")]
-    public void Part1_ShouldReturnInteger_WhenSample(AocInput input, int expected)
+    public void Part1(AocInput input, long expected)
     {
         // Act
         var result = Day06.Part1(input);
@@ -39,8 +39,8 @@ public sealed class Day06Tests
     
     [Theory]
     [MemberData(nameof(Part2Data))]
-    [Description("<insert here>")]
-    public void Part2_ShouldReturnInteger_WhenSample(AocInput input, int expected)
+    [Description("How many ways can you beat the record in this one much longer race?")]
+    public void Part2(AocInput input, long expected)
     {
         // Act
         var result = Day06.Part2(input);
