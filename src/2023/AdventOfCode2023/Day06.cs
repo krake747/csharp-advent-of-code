349e0527264598a87a2784d2adbd422e0cec001f
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-12-06 19:22:47 +0100

    refactor(2023): Day 6 part 1

--- src/2023/AdventOfCode2023/Day06.cs
+++ src/2023/AdventOfCode2023/Day06.cs
@@ -1,4 +1,4 @@
-using System.Text.RegularExpressions;
+﻿using System.Text.RegularExpressions;
 using AdventOfCodeLib;
 
 namespace AdventOfCode2023;
@@ -6,26 +6,17 @@ namespace AdventOfCode2023;
 [AocPuzzle(2023, 6, "Wait For It")]
 public sealed partial class Day06 : IAocDay<int>
 {
-    public static int Part1(AocInput input)
-    {
-        var races = ParseRaces(input.Lines);
-        var distances = races.Select(race =>
-        {
-            List<bool> distances = [];
-            foreach (var hold in Enumerable.Range(0, race.Time + 1))
-            {
-                var boatSpeed = hold;
-                var travelTime = race.Time - hold;
-                var travel = boatSpeed * travelTime;
-                distances.Add(travel > race.RecordDistance);
-            }
-            
-            return distances.Count(x => x is true);
-        });
-        
-        
-        return distances.Aggregate(1, (seed, acc) => seed * acc);
-    }
+    public static int Part1(AocInput input) =>
+        ParseRaces(input.Lines)
+            .Select(BeatingTheRecord)
+            .Aggregate(1, (wins, ways) => wins * ways);
+
+    private static int BeatingTheRecord(Race race) =>
+        Enumerable.Range(0, race.Time + 1)
+            .Select(hold => (BoatSpeed: hold, TravelTime: race.Time - hold))
+            .Select(x => x.BoatSpeed * x.TravelTime)
+            .Select(travel => travel > race.RecordDistance)
+            .Count(x => x);
 
     private static IEnumerable<Race> ParseRaces(IEnumerable<string> lines) => lines
         .Select(line => NumbersRegex().Matches(line).Select(m => int.Parse(m.Value)))
