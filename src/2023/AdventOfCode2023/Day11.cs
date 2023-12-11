a83573a20bcc941d7425bb47964e57a3ee831a4c
Kevin Kraemer <kraemer.kevin747@gmail.com>
2023-12-11 13:25:09 +0100

    feat(2023): solve day 11 part 2

--- src/2023/AdventOfCode2023/Day11.cs
+++ src/2023/AdventOfCode2023/Day11.cs
@@ -3,9 +3,9 @@ using AdventOfCodeLib;
 namespace AdventOfCode2023;
 
 [AocPuzzle(2023, 11, "Cosmic Expansion")]
-public sealed class Day11 : IAocDay<int>
+public sealed class Day11 : IAocDay<long>
 {
-    public static int Part1(AocInput input)
+    public static long Part1(AocInput input)
     {
         var galaxies = ParseGalaxies(input.AllLines).ToArray();
         var emptyRows = EmptySpaceRows(input.AllLines).ToArray();
@@ -14,8 +14,25 @@ public sealed class Day11 : IAocDay<int>
 
         var sum = galaxyParis.Sum(p =>
         {
-            var distRow = Distance(p.g1.Row, p.g2.Row) + ExpansionRow(p.g1, p.g2, emptyRows);
-            var distCol = Distance(p.g1.Col, p.g2.Col) + ExpansionCol(p.g1, p.g2, emptyCols);
+            var distRow = Distance(p.g1.Row, p.g2.Row) + ExpansionRow(p.g1, p.g2, 1, emptyRows);
+            var distCol = Distance(p.g1.Col, p.g2.Col) + ExpansionCol(p.g1, p.g2, 1, emptyCols);
+            return distRow + distCol;
+        }) / 2; // take half from cartesian input
+        
+        return sum;
+    }
+    
+    public static long Part2(AocInput input)
+    {
+        var galaxies = ParseGalaxies(input.AllLines).ToArray();
+        var emptyRows = EmptySpaceRows(input.AllLines).ToArray();
+        var emptyCols = EmptySpaceCols(input.AllLines).ToArray();
+        var galaxyParis = galaxies.SelectMany(_ => galaxies, (g1, g2) => (g1, g2));
+
+        var sum = galaxyParis.Sum(p =>
+        {
+            var distRow = Distance(p.g1.Row, p.g2.Row) + ExpansionRow(p.g1, p.g2, 999999, emptyRows);
+            var distCol = Distance(p.g1.Col, p.g2.Col) + ExpansionCol(p.g1, p.g2, 999999, emptyCols);
             return distRow + distCol;
         }) / 2; // take half from cartesian input
         
@@ -24,23 +41,21 @@ public sealed class Day11 : IAocDay<int>
 
     private static int Distance(int p1, int p2) => Math.Abs(p1 - p2);
     
-    private static int ExpansionRow(Galaxy g1, Galaxy g2, IEnumerable<int> emptySpaces)
+    private static long ExpansionRow(Galaxy g1, Galaxy g2, int expansion, IEnumerable<int> emptySpaces)
     {
         var ((r1, _), (r2, _)) = (g1, g2);
         var start = Math.Min(r1, r2);
         var dist = Distance(r1, r2);
-        return 1 * Enumerable.Range(start, dist).Count(x => emptySpaces.Contains(x));
+        return expansion * Enumerable.Range(start, dist).Count(x => emptySpaces.Contains(x));
     }
     
-    private static int ExpansionCol(Galaxy g1, Galaxy g2, IEnumerable<int> emptySpaces)
+    private static int ExpansionCol(Galaxy g1, Galaxy g2, int expansion, IEnumerable<int> emptySpaces)
     {
         var ((_, c1), (_, c2)) = (g1, g2);
         var start = Math.Min(c1, c2);
         var dist = Distance(c1, c2);
-        return 1 * Enumerable.Range(start, dist).Count(x => emptySpaces.Contains(x));
+        return expansion * Enumerable.Range(start, dist).Count(x => emptySpaces.Contains(x));
     }
-    
-    public static int Part2(AocInput input) => 0;
     
     private static IEnumerable<Galaxy> ParseGalaxies(string[] universe)
     {
--- tests/2023/AdventOfCode2023.Tests.Unit/Day11Tests.cs
+++ tests/2023/AdventOfCode2023.Tests.Unit/Day11Tests.cs
@@ -6,29 +6,29 @@ using static AdventOfCodeLib.AocFileReaderService;
 namespace AdventOfCode2023.Tests.Unit;
 
 [AocPuzzle(2023, 11, "Cosmic Expansion")]
-public sealed class Day11Tests : IAocDayTest<int>
+public sealed class Day11Tests : IAocDayTest<long>
 {
     private const string Day = nameof(Day11);
     private const string TestData = @$"..\..\..\Data\{Day}_Test.txt";
     private const string RealData = @$"..\..\..\Data\{Day}.txt";
     private readonly Day11 _sut = new();
 
-    public static TheoryData<AocInput, int> Part1Data => new()
+    public static TheoryData<AocInput, long> Part1Data => new()
     {
         { ReadInput(TestData), 374 },
         { ReadInput(RealData), 9799681 }
     };
 
-    public static TheoryData<AocInput, int> Part2Data => new()
+    public static TheoryData<AocInput, long> Part2Data => new()
     {
-        { ReadInput(TestData), 0 },
-        { ReadInput(RealData), 0 }
+        { ReadInput(TestData), 82000210 },
+        { ReadInput(RealData), 513171773355 }
     };
 
     [Theory]
     [MemberData(nameof(Part1Data))]
     [Description("What is the sum of these lengths?")]
-    public void Part1(AocInput input, int expected)
+    public void Part1(AocInput input, long expected)
     {
         // Act
         var result = Day11.Part1(input);
@@ -40,7 +40,7 @@ public sealed class Day11Tests : IAocDayTest<int>
     [Theory]
     [MemberData(nameof(Part2Data))]
     [Description("<insert question 2 here>")]
-    public void Part2(AocInput input, int expected)
+    public void Part2(AocInput input, long expected)
     {
         // Act
         var result = Day11.Part2(input);
