44868c7096e5d00324c9478699d982cc49a1e264
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-05-31 14:49:09 +0200

    refactor(dotnet, 2015): Rename Points -> Point

--- dotnet/2015/AdventOfCode.Y2015/Day03.cs
+++ dotnet/2015/AdventOfCode.Y2015/Day03.cs
@@ -6,10 +6,10 @@ namespace AdventOfCode.Y2015;
 public sealed class Day03 : IAocDay<int>
 {
     public static int Part1(AocInput input) => input.Text
-        .Aggregate(new List<Points> { new(0, 0) }, (visited, x) =>
+        .Aggregate(new List<Point> { new(0, 0) }, (visited, x) =>
         {
             var currentHouse = visited[^1];
-            var nextHouse = Points.MoveToNextHouse(x, currentHouse);
+            var nextHouse = Point.MoveToNextHouse(x, currentHouse);
             visited.Add(nextHouse);
             return visited;
         })
@@ -18,11 +18,11 @@ public sealed class Day03 : IAocDay<int>
 
     public static int Part2(AocInput input) => input.Text
         .Select((x, i) => (Direction: x, Index: i))
-        .Aggregate((List<List<Points>>) [[new Points(0, 0)], [new Points(0, 0)]], (visited, x) =>
+        .Aggregate((List<List<Point>>) [[new Point(0, 0)], [new Point(0, 0)]], (visited, x) =>
         {
             var isSanta = x.Index % 2 == 0 ? 0 : 1;
             var currentHouse = visited[isSanta][^1];
-            var nextHouse = Points.MoveToNextHouse(x.Direction, currentHouse);
+            var nextHouse = Point.MoveToNextHouse(x.Direction, currentHouse);
             visited[isSanta].Add(nextHouse);
 
             return visited;
@@ -31,9 +31,9 @@ public sealed class Day03 : IAocDay<int>
         .ToHashSet()
         .Count;
 
-    private readonly record struct Points(int X, int Y)
+    private readonly record struct Point(int X, int Y)
     {
-        internal static Points MoveToNextHouse(char x, Points current) => x switch
+        internal static Point MoveToNextHouse(char x, Point current) => x switch
         {
             '^' => current with { Y = current.Y + 1 },
             '>' => current with { X = current.X + 1 },
