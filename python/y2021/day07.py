f5edf1cd398013a0df115c8bbcb338c32d20cc7a
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-06-01 19:48:36 +0200

    feat(python, 2021): Solve day 7

---
+++ python/y2021/day07.py
@@ -0,0 +1,25 @@
+from aoc.core import solve, AocInput
+
+
+def leastFuelConstantRate(crabs: list[int]) -> int:
+    return min(sum([abs(c - h) for c in crabs]) for h in range(0, max(crabs)))
+
+
+def leastFuelVariableRate(crabs: list[int]) -> int:
+    return min(sum(map(lambda n: int(n * (n + 1) / 2), [abs(c - h) for c in crabs])) for h in range(0, max(crabs)))
+
+
+def part1(input: AocInput) -> int:
+    return leastFuelConstantRate(list(map(int, input.text.split(","))))
+
+
+def part2(input: AocInput) -> int:
+    return leastFuelVariableRate(list(map(int, input.text.split(","))))
+
+
+def main() -> None:
+    YEAR = "y2021"
+    DAY = "Day07"
+    print(f"Running year {YEAR.replace('y', '')} day {DAY.replace('Day', '')}")
+    solve(YEAR, DAY, part1)
+    solve(YEAR, DAY, part2)
