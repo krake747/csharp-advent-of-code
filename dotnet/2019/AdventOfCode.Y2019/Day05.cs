d84fad7ad501029c81a8cf2fe2f3731ef4140ae8
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-06-02 17:23:48 +0200

    feat(dotnet, 2019): Init day 5

---
+++ dotnet/2019/AdventOfCode.Y2019/Day05.cs
@@ -0,0 +1,11 @@
+using AdventOfCode.Lib;
+
+namespace AdventOfCode.Y2019;
+
+[AocPuzzle(2019, 5, "Sunny with a Chance of Asteroids")]
+public sealed class Day05 : IAocDay<int>
+{
+    public static int Part1(AocInput input) => 0;
+
+    public static int Part2(AocInput input) => 0;
+}
