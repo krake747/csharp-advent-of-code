bafa32b439c75920aba3fbccdb58d66a1c2aead1
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-06-02 13:18:57 +0200

    feat(dotnet, 2019): Init day 2

---
+++ dotnet/2019/AdventOfCode.Y2019.Tests.Unit/Day02Tests.cs
@@ -0,0 +1,50 @@
+using System.ComponentModel;
+using AdventOfCode.Lib;
+using static AdventOfCode.Lib.AocFileReaderService;
+
+namespace AdventOfCode.Y2019.Tests.Unit;
+
+[AocPuzzle(2019, 2, "1202 Program Alarm")]
+public sealed class Day02Tests : IAocDayTest<int>
+{
+    private const string Day = nameof(Day02);
+    private const string TestData = @$"..\..\..\Data\{Day}_Test.txt";
+    private const string RealData = @$"..\..\..\Data\{Day}.txt";
+    private readonly Day02 _sut = new();
+
+    public static TheoryData<AocInput, int> Part1Data => new()
+    {
+        { ReadInput(TestData), 0 },
+        { ReadInput(RealData), 0 }
+    };
+
+    public static TheoryData<AocInput, int> Part2Data => new()
+    {
+        { ReadInput(TestData), 0 },
+        { ReadInput(RealData), 0 }
+    };
+
+    [Theory]
+    [MemberData(nameof(Part1Data))]
+    [Description("What value is left at position 0 after the program halts?")]
+    public void Part1(AocInput input, int expected)
+    {
+        // Act
+        var result = Day02.Part1(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+    
+    [Theory]
+    [MemberData(nameof(Part2Data))]
+    [Description("<insert question 2 here>")]
+    public void Part2(AocInput input, int expected)
+    {
+        // Act
+        var result = Day02.Part2(input);
+    
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ dotnet/2019/AdventOfCode.Y2019/Day02.cs
@@ -0,0 +1,11 @@
+using AdventOfCode.Lib;
+
+namespace AdventOfCode.Y2019;
+
+[AocPuzzle(2019, 2, "1202 Program Alarm")]
+public sealed class Day02 : IAocDay<int>
+{
+    public static int Part1(AocInput input) => 0;
+
+    public static int Part2(AocInput input) => 0;
+}
