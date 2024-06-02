0502133c440683a7bb9cf5622c5f23e11d407c9f
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-06-02 14:18:55 +0200

    feat(dotnet, 2019): Init day 3

---
+++ dotnet/2019/AdventOfCode.Y2019.Tests.Unit/Day03Tests.cs
@@ -0,0 +1,51 @@
+using System.ComponentModel;
+using AdventOfCode.Lib;
+using FluentAssertions;
+using static AdventOfCode.Lib.AocFileReaderService;
+
+namespace AdventOfCode.Y2019.Tests.Unit;
+
+[AocPuzzle(2019, 3, "Crossed Wires")]
+public sealed class Day03Tests : IAocDayTest<int>
+{
+    private const string Day = nameof(Day03);
+    private const string TestData = @$"..\..\..\Data\{Day}_Test.txt";
+    private const string RealData = @$"..\..\..\Data\{Day}.txt";
+    private readonly Day03 _sut = new();
+
+    public static TheoryData<AocInput, int> Part1Data => new()
+    {
+        { ReadInput(TestData), 0 },
+        { ReadInput(RealData), 0 }
+    };
+
+    public static TheoryData<AocInput, int> Part2Data => new()
+    {
+        { ReadInput(TestData), 0 },
+        { ReadInput(RealData), 0 }
+    };
+
+    [Theory]
+    [MemberData(nameof(Part1Data))]
+    [Description("")]
+    public void Part1(AocInput input, int expected)
+    {
+        // Act
+        var result = Day03.Part1(input);
+
+        // Assert
+        result.Should().Be(expected);
+    }
+    
+    [Theory]
+    [MemberData(nameof(Part2Data))]
+    [Description("<insert question 2 here>")]
+    public void Part2(AocInput input, int expected)
+    {
+        // Act
+        var result = Day03.Part2(input);
+    
+        // Assert
+        result.Should().Be(expected);
+    }
+}
---
+++ dotnet/2019/AdventOfCode.Y2019/Day03.cs
@@ -0,0 +1,11 @@
+using AdventOfCode.Lib;
+
+namespace AdventOfCode.Y2019;
+
+[AocPuzzle(2019, 3, "Crossed Wires")]
+public sealed class Day03 : IAocDay<int>
+{
+    public static int Part1(AocInput input) => 0;
+
+    public static int Part2(AocInput input) => 0;
+}
