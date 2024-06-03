ca6bb4746a86fa56befa15a6ff375bf8445184f3
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-06-03 11:13:44 +0200

    feat(dotnet, 2019): Init day 6

---
+++ dotnet/2019/AdventOfCode.Y2019/Day06.cs
@@ -0,0 +1,11 @@
+using AdventOfCode.Lib;
+
+namespace AdventOfCode.Y2019;
+
+[AocPuzzle(2019, 6, "Universal Orbit Map")]
+public sealed class Day06 : IAocDay<int>
+{
+    public static int Part1(AocInput input) => 0;
+
+    public static int Part2(AocInput input) => 0;
+}
