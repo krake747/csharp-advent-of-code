40636c41bca1597535b10564673f133e610613a9
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-12-01 11:46:15 +0100

    refactor(csharp, 2024): day 1

--- dotnet/2024/AdventOfCode.Y2024/Day01.cs
+++ dotnet/2024/AdventOfCode.Y2024/Day01.cs
@@ -1,45 +1,29 @@
-using System.Text.RegularExpressions;
 using AdventOfCode.Lib;
-using Instructions = ( 
-    System.Collections.Generic.IEnumerable<int> Left, 
-    System.Collections.Generic.IEnumerable<int> Right
-);
 
 namespace AdventOfCode.Y2024;
 
-[AocPuzzle(2024, 1, "Historian Hysteria")]
-public sealed partial class Day01 : IAocDay<int>
+[AocPuzzle(2024, 1, "Historian Hysteria", "C#")]
+public sealed class Day01 : IAocDay<int>
 {
     public static int Part1(AocInput input) => input.AllLines
-        .Pipe(ParseInstructions)
-        .Pipe(instructions =>
-            instructions.Left.Order().Zip(instructions.Right.Order(), (l, r) => Math.Abs(l - r)).Sum());
+        .Pipe(lines =>
+        {
+            var left = ParseInstructions(lines, 0);
+            var right = ParseInstructions(lines, 1);
+            return left.Order().Zip(right.Order(), (l, r) => Math.Abs(l - r)).Sum();
+        });
 
     public static int Part2(AocInput input) => input.AllLines
-        .Pipe(ParseInstructions)
-        .Pipe(SimilarityScore);
-
-    private static int SimilarityScore(Instructions instructions)
-    {
-        var counts = instructions.Right.CountBy(i => i).ToDictionary();
-        return instructions.Left.Sum(id => counts.GetValueOrDefault(id) * id);
-    }
-
-    private static Instructions ParseInstructions(string[] lines)
-    {
-        (int Left, int Right)[] matches =
-        [
-            ..lines
-                .Select(l => NumbersRegex().Match(l))
-                .Select(m => (Left: int.Parse(m.Groups[1].Value), Right: int.Parse(m.Groups[2].Value)))
-        ];
-
-        return (
-            Left: matches.Select(m => m.Left),
-            Right: matches.Select(m => m.Right)
-        );
-    }
-
-    [GeneratedRegex(@"(\d+)\s*(\d+)", RegexOptions.Compiled | RegexOptions.NonBacktracking)]
-    private static partial Regex NumbersRegex();
+        .Pipe(lines =>
+        {
+            var left = ParseInstructions(lines, 0);
+            var counts = ParseInstructions(lines, 1).CountBy(i => i).ToDictionary();
+            return left.Sum(id => counts.GetValueOrDefault(id) * id);
+        });
+    
+    private static IEnumerable<int> ParseInstructions(IEnumerable<string> lines, int col) =>
+        from line in lines
+        let nums = line.Split("   ").Select(int.Parse).ToArray()
+        orderby nums[col]
+        select nums[col];
 }
