03904fc731726ffbc669b5470bc54bd46f02d540
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-12-03 11:24:42 +0100

    refactor(csharp, 2024): day 3

--- dotnet/2024/AdventOfCode.Y2024/Day03.cs
+++ dotnet/2024/AdventOfCode.Y2024/Day03.cs
@@ -1,32 +1,35 @@
 using System.Text.RegularExpressions;
 using AdventOfCode.Lib;
+using State = (bool Enabled, long Result);
 
 namespace AdventOfCode.Y2024;
 
 [AocPuzzle(2024, 3, "Mull It Over", "C#")]
-public sealed class Day03 : IAocDay<long>
+public sealed partial class Day03 : IAocDay<long>
 {
-    public static long Part1(AocInput input) => input.Text
-        .Pipe(
-            text => Regex.Matches(text, @"mul\((\d+),(\d+)\)")
-                .Sum(m => int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value))
-        );
+    public static long Part1(AocInput input) =>
+        input.Text.Pipe(text => ComputerMemory().Matches(text).Sum(Instructions));
 
     public static long Part2(AocInput input) => input.Text
-        .Pipe(
-            text => Regex.Matches(text, @"do\(\)|don't\(\)|mul\((\d+),(\d+)\)")
-                .Aggregate(new State(true, 0), (state, m) => m.Value switch
+        .Pipe(text => ComputerMemoryWithConditionals().Matches(text)
+            .Aggregate(
+                new State(true, 0),
+                (state, m) => m.Value switch
                 {
                     "do()" => state with { Enabled = true },
                     "don't()" => state with { Enabled = false },
-                    _ => state.Enabled
-                        ? state with
-                        {
-                            Result = state.Result + int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value)
-                        }
-                        : state
-                }).Result
-        );
+                    _ => state.Enabled ? state with { Result = state.Result + Instructions(m) } : state
+                }
+            )
+        )
+        .Pipe(state => state.Result);
 
-    private readonly record struct State(bool Enabled, long Result);
+    private static int Instructions(Match m) =>
+        int.Parse(m.Groups[1].Value) * int.Parse(m.Groups[2].Value);
+
+    [GeneratedRegex(@"mul\((\d+),(\d+)\)", RegexOptions.Compiled | RegexOptions.NonBacktracking)]
+    private static partial Regex ComputerMemory();
+
+    [GeneratedRegex(@"do\(\)|don't\(\)|mul\((\d+),(\d+)\)", RegexOptions.Compiled | RegexOptions.NonBacktracking)]
+    private static partial Regex ComputerMemoryWithConditionals();
 }
