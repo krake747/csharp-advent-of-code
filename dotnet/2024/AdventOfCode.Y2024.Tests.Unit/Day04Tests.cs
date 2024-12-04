24ac4b84ddc427021daa3557ba3dbf2c6e3b8257
Kevin Kraemer <kraemer.kevin747@gmail.com>
2024-12-04 12:11:19 +0100

    feat(csharp, 2024): solve day 4

---
+++ dotnet/2024/AdventOfCode.Y2024.Tests.Unit/Day04Tests.cs
@@ -0,0 +1,46 @@
+using System.ComponentModel;
+using AdventOfCode.Lib;
+using FluentAssertions;
+using static AdventOfCode.Lib.AocFileReaderService;
+
+namespace AdventOfCode.Y2024.Tests.Unit;
+
+[AocPuzzle(2024, 4, "Ceres Search", "C#")]
+public sealed class Day04Tests : IAocDayTest<int>
+{
+    private const string Day = nameof(Day04);
+    private const string TestData = @$"..\..\..\Data\{Day}_Test.txt";
+    private const string RealData = @$"..\..\..\Data\{Day}.txt";
+
+    public static TheoryData<AocInput, int> Part1Data => new()
+    {
+        { ReadInput(TestData), 18 },
+        { ReadInput(RealData), 2358 }
+    };
+
+    public static TheoryData<AocInput, int> Part2Data => new()
+    {
+        { ReadInput(TestData), 9 },
+        { ReadInput(RealData), 1737 }
+    };
+
+    [Theory]
+    [MemberData(nameof(Part1Data))]
+    [Description("How many times does XMAS appear?")]
+    public void Part1(AocInput input, int expected)
+    {
+        var result = Day04.Part1(input);
+
+        result.Should().Be(expected);
+    }
+    
+    [Theory]
+    [MemberData(nameof(Part2Data))]
+    [Description("How many times does an X-MAS appear?")]
+    public void Part2(AocInput input, int expected)
+    {
+        var result = Day04.Part2(input);
+    
+        result.Should().Be(expected);
+    }
+}
---
+++ dotnet/2024/AdventOfCode.Y2024/Day04.cs
@@ -0,0 +1,63 @@
+using System.Collections.Frozen;
+using System.Numerics;
+using AdventOfCode.Lib;
+using Map = System.Collections.Frozen.FrozenDictionary<AdventOfCode.Y2024.Point, char>;
+
+namespace AdventOfCode.Y2024;
+
+[AocPuzzle(2024, 4, "Ceres Search", "C#")]
+public sealed class Day04 : IAocDay<int>
+{
+    public static int Part1(AocInput input) =>
+        input.AllLines
+            .Pipe(ParseMap)
+            .Pipe(
+                static map =>
+                    from point in map.Keys
+                    from direction in new[] { Point.East, Point.SouthEast, Point.South, Point.SouthWest }
+                    select WordSearchMatches(map, point, direction, "XMAS")
+            )
+            .Pipe(matches => matches.Count(m => m));
+
+    public static int Part2(AocInput input) =>
+        input.AllLines
+            .Pipe(ParseMap)
+            .Pipe(
+                map =>
+                    from point in map.Keys
+                    select WordSearchMatches(map, point + Point.NorthWest, Point.SouthEast, "MAS") &&
+                           WordSearchMatches(map, point + Point.SouthWest, Point.NorthEast, "MAS")
+            )
+            .Pipe(matches => matches.Count(m => m));
+
+    private static bool WordSearchMatches(Map map, Point point, Point direction, string pattern)
+    {
+        var len = pattern.Length;
+        char[] chars = [..Enumerable.Range(0, len).Select(i => map.GetValueOrDefault(point + direction * i))];
+        return chars.SequenceEqual(pattern) || chars.SequenceEqual(pattern.Reverse());
+    }
+
+    private static Map ParseMap(string[] lines) =>
+    (
+        from y in Enumerable.Range(0, lines.Length)
+        from x in Enumerable.Range(0, lines[0].Length)
+        select KeyValuePair.Create(new Point(x, y), lines[y][x])
+    ).ToFrozenDictionary();
+}
+
+internal readonly record struct Point(int X, int Y)
+    : IAdditionOperators<Point, Point, Point>,
+        IMultiplyOperators<Point, int, Point>
+{
+    public static readonly Point North = new(0, -1);      // Move up
+    public static readonly Point NorthEast = new(1, -1);  // Move diagonally up-right
+    public static readonly Point East = new(1, 0);        // Move right
+    public static readonly Point SouthEast = new(1, 1);   // Move diagonally down-right
+    public static readonly Point South = new(0, 1);       // Move down
+    public static readonly Point SouthWest = new(-1, 1);  // Move diagonally down-left
+    public static readonly Point West = new(-1, 0);       // Move left
+    public static readonly Point NorthWest = new(-1, -1); // Move diagonally up-left
+
+    public static Point operator +(Point p1, Point p2) => new(p1.X + p2.X, p1.Y + p2.Y);
+    public static Point operator *(Point p, int factor) => new(p.X * factor, p.Y * factor);
+}
